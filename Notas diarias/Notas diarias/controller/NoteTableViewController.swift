8a27cbc178072ccc1b9c568d8009efb5667203d0
Gilberto Silva <gilberto.silva@sigga.com>
2021-03-14 17:04:07 -0300

    Ajustes de indentação.

--- Notas diarias/Notas diarias/controller/NoteTableViewController.swift
+++ Notas diarias/Notas diarias/controller/NoteTableViewController.swift
@@ -20,11 +20,6 @@ class NoteTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.rowHeight = 125
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -72,69 +67,23 @@ class NoteTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-            let currentNote = notes[indexPath.row]
-            if editingStyle == .delete {
-                deleteCell(currentNote)
-            }
-        }
-        private func deleteCell(_ currentNote: NoteModel) {
-            let message = "Deseja deletar a nota: \(currentNote.text)?"
-            let positiveHandler:((UIAlertAction) -> Void)? = { (action) in
-                let isSuccess = self.noteRepository.delete(note: currentNote)
-                if isSuccess {
-                    AlertHelper.shared.showMessage(viewController: self, message: "Nota deletada com sucesso!")
-                    self.loadNotes()
-                }else {
-                    AlertHelper.shared.showMessage(viewController: self, message: "Error ao deletar nota!")
-                }
-            }
-            AlertHelper.shared.showConfirmationMessage(viewController: self, message: message, positiveHandler: positiveHandler)
-        }
-
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        let currentNote = notes[indexPath.row]
         if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            deleteCell(currentNote)
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    private func deleteCell(_ currentNote: NoteModel) {
+        let message = "Deseja deletar a nota: \(currentNote.text)?"
+        let positiveHandler:((UIAlertAction) -> Void)? = { (action) in
+            let isSuccess = self.noteRepository.delete(note: currentNote)
+            if isSuccess {
+                AlertHelper.shared.showMessage(viewController: self, message: "Nota deletada com sucesso!")
+                self.loadNotes()
+            }else {
+                AlertHelper.shared.showMessage(viewController: self, message: "Error ao deletar nota!")
+            }
+        }
+        AlertHelper.shared.showConfirmationMessage(viewController: self, message: message, positiveHandler: positiveHandler)
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
